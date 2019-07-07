e34d5a0ca53cf9442e43e3ef3b5a369b5faf8901
thecodeshool-niraj <learnerniraj@gmail.com>
2019-07-07 11:55:30 +0530

    sample stubbed user list

---
+++ src/main/java/com/thecodeschool/aws/SampleUserListApplication.java
@@ -0,0 +1,13 @@
+package com.thecodeschool.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleUserListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleUserListApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/thecodeschool/aws/model/User.java
@@ -0,0 +1,42 @@
+package com.thecodeschool.aws.model;
+
+public class User {
+
+	private long id;
+	private String firstName;
+	private String lastName;
+	
+	public User() {
+		this.id = -1;
+	}
+	
+	public User(long id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
