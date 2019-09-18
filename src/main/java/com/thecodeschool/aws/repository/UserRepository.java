65ce7f87882783e28cb354a94d0c4dfe676d5389
thecodeschool-niraj <learnerniraj@gmail.com>
2019-09-18 13:47:38 +0530

    deleted directories and files after restructuring

--- src/main/java/com/thecodeschool/aws/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thecodeschool.aws.repository;
-import com.thecodeschool.aws.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
